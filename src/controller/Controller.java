681b7cab60bb24b2cd1e28fff6a73297ba3b24a9
Zachary <zachary@MacBook-Pro-de-Zachary.local>
2018-11-06 21:29:11 -0500

    changes to Controller and addition of Test folder

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -14,50 +14,17 @@ public class Controller {
 	private View view;
 	private AbstractAction updateAction;
 	private Collection<String> questionPool;
-	
-	public Timer getT() {
-		return t;
-	}
-
-	public void setT(Timer t) {
-		this.t = t;
-	}
-
-	public Model getModel() {
-		return model;
-	}
-
-	public void setModel(Model model) {
-		this.model = model;
-	}
-
-	public View getView() {
-		return view;
-	}
-
-	public void setView(View view) {
-		this.view = view;
-	}
-
-	public AbstractAction getUpdateAction() {
-		return updateAction;
-	}
-
-	public void setUpdateAction(AbstractAction updateAction) {
-		this.updateAction = updateAction;
-	}
-
-	public Collection<String> getQuestionPool() {
-		return questionPool;
-	}
-
-	public void setQuestionPool(Collection<String> questionPool) {
-		this.questionPool = questionPool;
+	private CustomMouseListner mouseListener;
+	public Controller(){
+		view= new View(mouseListener);
+		model= new Model(view.getFrameWidth(),view.getFrameHeight(),model/*some model*/); 
 	}
 
 	private void start() {
 		// TODO Auto-generated method stub
-
+		// t=new Timer(drawDelay, updateAction);
+		// t.start();
 	}
+	
 
 }
