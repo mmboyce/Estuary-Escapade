878be5738308f3759a4e19fe09d4629c67ec7bdd
Miguel-Fuentes <Miguel.Fuentes.new@gmail.com>
2018-11-26 09:21:50 -0500

    commented ViewContainer

--- src/view/ViewContainer.java
+++ src/view/ViewContainer.java
@@ -24,25 +24,33 @@ public class ViewContainer {
 		frame.setUndecorated(true); // This removes the window border
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		// These get the size of the screen
 		width = frame.getContentPane().getWidth();
 		height = frame.getContentPane().getHeight();
 	}
 
 	public void initialize(MouseListener m, CodeListener c, ArrayList<GameObject> o) {
+		/*
+		 * This adds the MouseListener to the frame and initializes the view, this has
+		 * to happen after the constructor because the model needs to be set up first
+		 * and that requires the size of the screen
+		 */
 		frame.addMouseListener(m);
 		view = new TitleView(title, width, height, c, o);
 	}
-	
+
 	public void next(ArrayList<GameObject> o) {
 		view = view.nextView(o);
 		resetView();
 	}
-	
+
 	public void timeUp(QuizModel model) {
+		// This is only called if the view is a ObjectView but view needs to be cast to
+		// access timeUp
 		view = ((ObjectView) view).timeUp(model.getQuestion());
 		resetView();
 	}
-	
+
 	private void resetView() {
 		// If this is not done the JFrame will not display properly and things will look
 		// wrong
@@ -51,24 +59,25 @@ public class ViewContainer {
 		frame.validate();
 		frame.repaint();
 	}
-	
+
 	public void start() {
 		frame.add(view);
 		frame.setVisible(true);
 	}
-	
+
 	public void repaint() {
 		frame.repaint();
 	}
-	
+
 	public void close() {
+		// Program is set to end on window Close so this will terminate everything
 		frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
 	}
-	
+
 	public boolean checkObjectView() {
-		return(view instanceof ObjectView);
+		return (view instanceof ObjectView);
 	}
-	
+
 	public int getWidth() {
 		return width;
 	}
