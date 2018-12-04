2cf70a10229481071383b4a7f52e2b3c8ccbf7ea
Andre <zachary@wifi-roaming-128-4-159-174.host.udel.edu>
2018-12-04 10:48:43 -0500

    removed second background line

--- src/view/ObjectView.java
+++ src/view/ObjectView.java
@@ -58,7 +58,7 @@ public abstract class ObjectView extends View {
 
 	@Override
 	public void paint(Graphics g) {
-		g.drawImage(createImage("images/underwater.png"), 0, 0, getWidth(), getHeight(), (ImageObserver) this);
+		g.drawImage(background, 0, 0, getWidth(), getHeight(), (ImageObserver) this);
 		// Start of experimental code
 		int xpos = 0;
 		int ypos = getHeight()*9/10;
@@ -89,7 +89,6 @@ public abstract class ObjectView extends View {
 			g.drawImage(createImage(object.getImagePath()), object.getxPos(), object.getyPos(), object.getxSize(),
 					object.getySize(), (ImageObserver) this);
 		}
-		g.drawImage(background, 0, 0, getWidth(), getHeight(), (ImageObserver) this);
 		timer.paint(g);
 		for (GameObject object : this.map.keySet()) {
 			if (object.isVisible()) {
