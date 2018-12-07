af7ea2e99032333cbb017fa0d5e77cbe095b62cf
wmathieu <wmathieu@udel.edu>
2018-12-06 21:08:05 -0500

    javadocs

--- src/controller/Code.java
+++ src/controller/Code.java
@@ -1,6 +1,72 @@
 package controller;
 
+/**
+ * 	Code is an enumeration of codes that the model and view can cause transitions
+ *  in the controller from one state to the next, or 
+ *  {@link Code#FLASHSCREEN trigger events.}
+ * 
+ * @author Miguel Fuentes
+ * @author Andre Green
+ * @author Devon Pirestani
+ * @see CodeListener#codeEmitted(Code)
+ */
 public enum Code {
-	// List of codes that the model and view can cause transitions in the controller
-	NEXT, EXIT, TIMEUP, STARTTIMER, WRONG, RIGHT, FLASHSCREEN, PAUSE, RESUME, TUTORIAL
+	
+	/**
+	 * NEXT is a code emitted when it is time to transition from one Model/View 
+	 * to the next.
+	 */
+	NEXT,
+	
+	/**
+	 * EXIT is a code emitted when the play exits the game by clicking the exit
+	 * button.
+	 */
+	EXIT, 
+	
+	/**
+	 * TIMEUP is a code emitted when the timer has run out. It will take the player
+	 * straight to the Quiz.
+	 */
+	TIMEUP, 
+	
+	/**
+	 * STARTTIMER is a code emitted when the player hits the <b>Start</b>
+	 * button and begins the game.
+	 */
+	STARTTIMER, 
+	
+	/**
+	 * WRONG is a code emitted when the player answers the quiz incorrectly.
+	 */
+	WRONG, 
+	
+	/**
+	 * RIGHT is a code emitted when the player answers the quiz correctly.
+	 */
+	RIGHT, 
+	
+	/**
+	 * FLASHSCREEN is a code emitted when the camera takes the photograph
+	 * of the fish, and causes a flash.
+	 */
+	FLASHSCREEN, 
+	
+	/**
+	 * PAUSE is a code emitted when the timer should pause due to a popup
+	 * appearing.
+	 */
+	PAUSE, 
+	
+	/**
+	 * RESUME is a code emitted when the timer should resume due to a popup
+	 * closing.
+	 */
+	RESUME, 
+	
+	/**
+	 * TUTORIAL is a code emitted when the <b>How to Play</b> button is pressed.
+	 * It starts the game in tutorial mode.
+	 */
+	TUTORIAL
 }
--- src/controller/CustomKeyListener.java
+++ src/controller/CustomKeyListener.java
@@ -7,16 +7,38 @@ import model.EstuaryModel;
 import model.Model;
 import model.ResearchModel;
 
+/**
+ * CustomKeyListener handles all {@link KeyEvent KeyEvents} during the game.
+ * <p>
+ * For some reason you must alt+tab out and back, then click on the game for the
+ * KeyEvents to occur. <b><i>Pretty much only used for debugging.</i><b>
+ * 
+ * @author W Mathieu Mimms-Boyce
+ * @author Miguel Fuentes
+ */
 public class CustomKeyListener implements KeyListener {
 	private Model model;
 	private boolean debugging;
 
-	// CURRENTLY ONLY KEYS WORK IF YOU ALT-TAB OUT AND COME BACK
-
+	/**
+	 * Constructor for the CustomKeyListener
+	 * 
+	 * @param m The model we are affecting with our keypresses
+	 */
 	public CustomKeyListener(Model m) {
 		this.model = m;
 	}
 
+	/**
+	 * Handles actions for when the key has been pressed.
+	 * <p>
+	 * The only keyPress checked for is
+	 * <ul>
+	 * 	<li><b>ESC</b> - <i>Exits game</i></li>
+	 * </ul>
+	 * 
+	 * @param e The KeyEvent
+	 */
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
@@ -26,6 +48,21 @@ public class CustomKeyListener implements KeyListener {
 		}
 	}
 
+	/**
+	 * Handles actions for when a key is released. These were placed here so
+	 * that holding a key down would not repeatedly perform an action
+	 * <p>
+	 * Debugging Keys
+	 * <ul>
+	 * 	<li><b>Shift + D</b> - <i>Toggles Debug</i></li>
+	 * 	<ul>
+	 * 		<li><b>Q</b> - <i>Catches current Target on Estuary Screen</i></li>
+	 * 		<li><b>W</b> - <i>Reseraches Fish on Research Screen</i></li>
+	 * 		<li><b>E</b> - <i>Skips to Quiz with all fishes caught</i></li>
+	 * 		<li><b>R</b> - <i>Skips to Quiz with current fishes caught </i></li>
+	 * 	</ul>
+	 * </ul>
+	 */
 	@Override
 	public void keyReleased(KeyEvent e) {
 		int key = e.getKeyCode();
@@ -74,19 +111,31 @@ public class CustomKeyListener implements KeyListener {
 		}
 	}
 
+	/* (non-Javadoc)
+	 * @see java.awt.event.KeyListener#keyTyped(java.awt.event.KeyEvent)
+	 */
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// this page intentionally left blank
 	}
 
+	/**
+	 * Toggles debugging to the opposite of what it presently is.
+	 */
 	public void toggleDebugging() {
 		this.debugging = !debugging;
 	}
 
+	/**
+	 * @return If debugging is enabled or not
+	 */
 	public boolean isDebugging() {
 		return debugging;
 	}
 
+	/**
+	 * @param model The model to switch to
+	 */
 	public void setModel(Model model) {
 		this.model = model;
 	}
