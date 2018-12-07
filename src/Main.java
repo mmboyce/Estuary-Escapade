2f6f882cd5bdfa7ce6d4372396f7425c9f686f88
wmathieu <wmathieu@udel.edu>
2018-12-07 02:24:56 -0500

    javadoc

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 import controller.Controller;
 
+/**
+ * This is the class that instantiates the controller and runs our game.
+ * 
+ * @author Devon Pirestani
+ * @author Miguel Fuentes
+ */
 public class Main {
+	/**
+	 * In the main method we instantiate the controller and
+	 * {@link Controller#start() start} it, which causes all of our game to run.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Controller controller = new Controller();
 		controller.start();
