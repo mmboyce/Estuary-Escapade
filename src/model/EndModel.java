f2621943678726f37fe0fe93f2f840963dd30d5b
Miguel-Fuentes <Miguel.Fuentes.new@gmail.com>
2018-12-06 23:05:26 -0500

    Added javadocs to the model package

--- src/model/EndModel.java
+++ src/model/EndModel.java
@@ -2,39 +2,73 @@ package model;
 
 import controller.CodeListener;
 
+/**
+ * This model holds the score and knows if the quiz was answered correctly
+ * 
+ * @author Miguel Fuentes
+ * @author W Mathieu Mimms-Boyce
+ * @author Andre Green
+ *
+ */
 public class EndModel extends Model {
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = -7351184289128317941L;
 	// The players final score
 	private int score;
 	private boolean quizCorrect;
 
+	/**
+	 * @return score
+	 */
 	public int getScore() {
 		return score;
 	}
 
+	/**
+	 * Sets {@link score}
+	 * 
+	 * @param score
+	 */
 	public void setScore(int score) {
 		this.score = score;
 	}
 
+	/**
+	 * This constructor calls the super constructor and sets score and quizCorrect
+	 * 
+	 * @param frameWidth  width of screen
+	 * @param frameHeight height of screen
+	 * @param listener    codelistener
+	 * @param score       your score
+	 * @param quizCorrect was the quiz answered correctly
+	 */
 	public EndModel(int frameWidth, int frameHeight, CodeListener listener, int score, boolean quizCorrect) {
 		super(frameWidth, frameHeight, listener);
 		this.score = score;
 		this.quizCorrect = quizCorrect;
 	}
-	
+
+	/**
+	 * This goes back to the title
+	 */
 	@Override
 	public Model nextModel() {
 		// Sets the next model back to the title screen
 		return new TitleModel(super.getFrameWidth(), super.getFrameHeight(), getListener());
 	}
 
+	/**
+	 * @return quizCorrect
+	 */
 	public boolean isQuizCorrect() {
 		return quizCorrect;
 	}
 
+	/**
+	 * Sets {@link quizCorrect}
+	 * 
+	 * @param quizCorrect
+	 */
 	public void setQuizCorrect(boolean quizCorrect) {
 		this.quizCorrect = quizCorrect;
 	}
